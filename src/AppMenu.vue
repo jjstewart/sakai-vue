a6928224a7c19cd00967089a18cd00a46b354943
John <jstewart78@protonmail.com>
2022-07-12 00:29:33 -0400

    Initial commit

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,9 +1,6 @@
 <template>
 	<div class="layout-menu-container">
 		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" @keydown="onKeyDown" />
-		<a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
-			<img alt="primeblocks" :src="bannerImage()" class="w-full" />
-		</a>
 	</div>
 </template>
 
@@ -24,9 +21,6 @@ export default {
 				nodeElement.click();
 				event.preventDefault();
 			}
-		},
-		bannerImage() {
-			return this.$appState.darkTheme ? 'images/banner-primeblocks-dark.png' : 'images/banner-primeblocks.png';
 		}
     },
 	computed: {
--- src/router.js
+++ src/router.js
@@ -13,81 +13,9 @@ const routes = [
                 component: () => import('./components/Dashboard.vue')
             },
             {
-                path: '/formlayout',
-                name: 'formlayout',
-                component: () => import('./components/FormLayoutDemo.vue')
-            },
-            {
-                path: '/input',
-                name: 'input',
-                component: () => import('./components/InputDemo.vue')
-            },
-            {
-                path: '/floatlabel',
-                name: 'floatlabel',
-                component: () => import('./components/FloatLabelDemo.vue')
-            },
-            {
-                path: '/invalidstate',
-                name: 'invalidstate',
-                component: () => import('./components/InvalidStateDemo.vue')
-            },
-            {
-                path: '/button',
-                name: 'button',
-                component: () => import('./components/ButtonDemo.vue')
-            },
-            {
                 path: '/table',
                 name: 'table',
                 component: () => import('./components/TableDemo.vue')
-            },
-            {
-                path: '/list',
-                name: 'list',
-                component: () => import('./components/ListDemo.vue')
-            },
-            {
-                path: '/tree',
-                name: 'tree',
-                component: () => import('./components/TreeDemo.vue')
-            },
-            {
-                path: '/panel',
-                name: 'panel',
-                component: () => import('./components/PanelsDemo.vue')
-            },
-            {
-                path: '/overlay',
-                name: 'overlay',
-                component: () => import('./components/OverlayDemo.vue')
-            },
-            {
-                path: '/media',
-                name: 'media',
-                component: () => import('./components/MediaDemo.vue')
-            },
-            {
-                path: '/menu',
-                component: () => import('./components/MenuDemo.vue'),
-                children: [
-                    {
-                        path: '',
-                        component: () => import('./components/menu/PersonalDemo.vue')
-                    },
-                    {
-                        path: '/menu/seat',
-                        component: () => import('./components/menu/SeatDemo.vue')
-                    },
-                    {
-                        path: '/menu/payment',
-                        component: () => import('./components/menu/PaymentDemo.vue')
-                    },
-                    {
-                        path: '/menu/confirmation',
-                        component: () => import('./components/menu/ConfirmationDemo.vue')
-                    },
-                ],
             },
             {
                 path: '/messages',
@@ -95,34 +23,9 @@ const routes = [
                 component: () => import('./components/MessagesDemo.vue')
             },
             {
-                path: '/file',
-                name: 'file',
-                component: () => import('./components/FileDemo.vue')
-            },
-            {
                 path: '/chart',
                 name: 'chart',
                 component: () => import('./components/ChartDemo.vue')
-            },
-            {
-                path: '/misc',
-                name: 'misc',
-                component: () => import('./components/MiscDemo.vue')
-            },
-            {
-                path: '/crud',
-                name: 'crud',
-                component: () => import('./pages/CrudDemo.vue')
-            },
-            {
-                path: '/timeline',
-                name: 'timeline',
-                component: () => import('./pages/TimelineDemo.vue')
-            },
-            {
-                path: '/empty',
-                name: 'empty',
-                component: () => import('./components/EmptyPage.vue')
             },
             {
                 path: '/documentation',
@@ -130,14 +33,9 @@ const routes = [
                 component: () => import('./components/Documentation.vue')
             },
             {
-                path: '/blocks',
-                name: 'blocks',
-                component: () => import('./components/BlocksDemo.vue')
-            },
-            {
-                path: '/icons',
-                name: 'icons',
-                component: () => import('./components/IconsDemo.vue')
+                path: '/inventory',
+                name: 'inventory',
+                component: () => import('./3PInventory/List.vue')
             }
         ]
     },
